5dfbc8a6f1c794014e45ebeb73cda1a27354f97f
161250213 <674527347@qq.com>
2018-08-26 22:38:43 +0800

    code:create

---
+++ b-ink/src/main/java/bink/bink/BInkApplication.java
@@ -0,0 +1,12 @@
+package bink.bink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BInkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BInkApplication.class, args);
+	}
+}
